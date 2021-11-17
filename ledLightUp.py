3807a2f4ca68ee9f5aa7912f85f422f5998f6cd9
loyaljadey <michael.jia26@gmail.com>
2021-11-17 15:28:33 -0800

    changed the lighting aspect of the code to a definition/method

--- ledLightUp.py
+++ ledLightUp.py
@@ -6,8 +6,10 @@ pin = 18
 
 pixels = neopixel.NeoPixel(machine.Pin(pin), LED_count)
 
-for i in range(LED_count):
-    pixels[i] = (255,0,0)
-    pixels.write()
-    time.sleep(0.5)
+
+def continuousStrip():
+    for i in range(LED_count):
+        pixels[i] = (255,0,0)
+        pixels.write()
+        time.sleep(0.5)
     
