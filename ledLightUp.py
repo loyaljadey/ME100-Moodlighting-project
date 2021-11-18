b24130928adb5beff5bf82687a4fcfa3e2c08fe4
loyaljadey <michael.jia26@gmail.com>
2021-11-17 16:20:51 -0800

    made a new definition/method to do the lighting for a rainyday

--- ledLightUp.py
+++ ledLightUp.py
@@ -1,7 +1,7 @@
 import machine, neopixel
 import time
 
-LED_count = 50
+LED_count = 300
 pin = 18
 
 pixels = neopixel.NeoPixel(machine.Pin(pin), LED_count)
@@ -12,4 +12,10 @@ def continuousStrip():
         pixels[i] = (255,0,0)
         pixels.write()
         time.sleep(0.5)
-    
+
+
+def rainyDay():
+    for i in range(LED_count):
+        pixels[i] = (255,0,0)
+        pixels.write()
+        time.sleep(0.25)
