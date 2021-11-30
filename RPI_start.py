7545e203811f2f04be7ee03791d983ba50ff5762
Xavier Barneclo <xavierbarneclo@berkeley.edu>
2021-11-29 18:19:43 -0800

    RPI autostart additions

---
+++ RPI_start.py
@@ -0,0 +1,6 @@
+import subprocess
+import time
+
+subprocess.call('sudo cp /home/pi/archive.txt /home/pi/.asoundrc', shell=True)
+time.sleep(1)
+subprocess.call('python /home/pi/ME100-Moodlighting-project.py', shell=True)
