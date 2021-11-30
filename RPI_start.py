b9ffca97712f89a90928b41a8d042df6ab1a80a0
Xavier Barneclo <xavierbarneclo@berkeley.edu>
2021-11-29 18:23:33 -0800

    auto start bug fix

--- RPI_start.py
+++ RPI_start.py
@@ -3,4 +3,4 @@ import time
 
 subprocess.call('sudo cp /home/pi/archive.txt /home/pi/.asoundrc', shell=True)
 time.sleep(1)
-subprocess.call('python /home/pi/ME100-Moodlighting-project.py', shell=True)
+subprocess.call('python /home/pi/ME100-Moodlighting-project/RPI_host.py', shell=True)
